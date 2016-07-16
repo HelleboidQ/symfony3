d2025c8efeff2a57088796d3e532b176e1c75820
Khalid Sookia <khalidsookia@gmail.com>
2016-07-16 02:24:23 +0200

    Added and configured fos user bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new IKNSA\AppBundle\IKNSAAppBundle(),
             new IKNSA\BlogBundle\IKNSABlogBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
